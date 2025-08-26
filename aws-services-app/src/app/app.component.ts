6e109dd5e59befa17f89e33f549cd31415b0e008
ymougenel <ymougenel@takima.fr>
2025-08-26 22:47:49 +0200

    refactor: move shell into components

--- aws-services-app/src/app/app.component.ts
+++ aws-services-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FooterComponent} from "./shell/footer/footer.component";
-import {HeaderComponent} from "./shell/header/header.component";
+import {FooterComponent} from "./components/shell/footer/footer.component";
+import {HeaderComponent} from "./components/shell/header/header.component";
 
 @Component({
   selector: 'app-root',
